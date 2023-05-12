0bd83b56d37b1c3f0067cb387db18908e881dd62
hiropk <kano@netlab.jp>
2023-05-12 04:24:11 +0000

    Make a basic User model(including secure password)

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,10 @@ require "test_helper"
 class UserTest < ActiveSupport::TestCase
 
   def setup
-    @user = User.new(name: "Example User", email: "user@example.com")
+    @user = User.new(name: "Example User",
+                    email: "user@example.com",
+                    password: "foobar",
+                    password_confirmation: "foobar")
   end
 
   test "should be valid" do
@@ -12,6 +15,53 @@ class UserTest < ActiveSupport::TestCase
 
   test "name should be present" do
     @user.name = "     "
+    assert_not @user.valid?
+  end
+
+  test "email should be present" do
+    @user.email = "     "
+    assert_not @user.valid?
+  end
+
+  test "name should not be too long" do
+    @user.name = "b" * 51
+    assert_not @user.valid?
+  end
+
+  test "email should not be too long" do
+    @user.email = "b" * 244 + "@example.com"
+    assert_not @user.valid?
+  end
+
+  test "email vakidation should reject invalid addresses" do
+    invalid_adresses = %w[user@example,com user_at_foo.org user.name@example. foo@bar_baz.com foo@bar+baz.com foo@bar..com]
+
+    invalid_adresses.each do |invalid_adress|
+      @user.email = invalid_adress
+      assert_not @user.valid?, "{#invalid_adress.inspect} should be invalid"
+    end
+  end
+
+  test "email address should be unique" do
+    duplicate_user = @user.dup
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+
+  test "email addresses should be saved as lowercase" do
+    mixed_case_email = "Foo@email.Com"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
+  end
+
+  test "password should be present (nonblank)" do
+    @user.password = @user.password_confirmation = " " * 6
+    assert_not @user.valid?
+  end
+
+  test "password should have a minimum length" do
+    @user.password = @user.password_confirmation = "a" * 5
     assert_not @user.valid?
   end
 end
