9c7c0a179adb77d2e97ac38d3b40d75b20e899c2
hiropk <kano@netlab.jp>
2023-05-22 11:52:41 +0900

    improve menu.js

--- app/javascript/custom/menu.js
+++ app/javascript/custom/menu.js
@@ -1,10 +1,22 @@
 //　　ヘッダーのメニュー操作
 
-document.addEventListener("turbo:load", function() {
-  let account = document.querySelector("#account");
-  account.addEventListener("click", function(event){
+//　トグルリスナーを追加する
+//addToggleListener(トグル機能を追加したいタグのid, トグル機能で表示するmenuをもつタグのid, )
+function addToggleListener(selected_id, menu_id, toggle_class) {
+  let selected_element = document.querySelector(`#${selected_id}`);
+  selected_element.addEventListener("click", function(event) {
     event.preventDefault();
-    let menu = document.querySelector("#dropdown-menu");
-    menu.classList.toggle("active");
+    let menu = document.querySelector(`#${menu_id}`);
+    menu.classList.toggle(toggle_class);
+    console.log(menu);
   });
-});
+}
+
+// クリックをリッスンするトグルリスナーを追加する
+document.addEventListener("turbo:load", function() {
+  //collapseがある　→　navbar-menuが壊れる。
+  //collapseがない　→ navbar-menuが生きる。
+  addToggleListener("hamburger", "navbar-menu", "collapse");
+  //activeを追加するとdropdown-menuが実行される。
+  addToggleListener("account", "dropdown-menu", "active");
+})
