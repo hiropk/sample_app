021e171fd40b0e25fb33abf0b58e04417534ec39
hiropk <kano@netlab.jp>
2023-05-22 13:01:20 +0900

    improve menu.js

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -6,20 +6,21 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     @user = users(:michael)
   end
 
-  test "login with invalid information" do
+  test "login with valid emial/invalid password" do
     get login_path
     assert_template 'sessions/new'
     post login_path, params: { session: { email: @user.email, password: "invalid" } }
-    assert_response :unprocessable_entity
+    assert_not is_logged_in?
     assert_template 'sessions/new'
     assert_not flash.empty?
     get root_path
     assert flash.empty?
   end
 
-  test "login with valid information" do 
+  test "login with valid information followed by logput" do 
     post login_path, params: {session: { email:    @user.email, 
                                          password: 'password' } }
+    assert is_logged_in?
     assert_redirected_to @user
     follow_redirect!
     assert_template 'users/show'
@@ -29,5 +30,13 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", logout_path
     #　メニューにはcurrent_userメソッドでログイン中のuserオブジェクトを返しており、profileにこのuser_path(@user)でリンク先を示している。
     assert_select "a[href=?]", user_path(@user)
+    delete logout_path
+    assert_not is_logged_in?
+    assert_response :see_other
+    assert_redirected_to root_path
+    follow_redirect!
+    assert_select "a[href=?]", login_path
+    assert_select "a[href=?]", logout_path,      count: 0
+    assert_select "a[href=?]", user_path(@user), count: 0
   end
 end
