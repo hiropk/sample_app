f537efd249281d049922d57d74fe899efa4edb89
hiropk <kano@netlab.jp>
2023-05-23 14:31:48 +0900

    Implement advanced login

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -19,15 +19,9 @@ module SessionsHelper
     elsif (user_id = cookies.encrypted[:user_id])
       user = User.find_by(id: user_id)
       if user && user.authenticated?(cookies[:remember_token])
-        log_in user@current_user = user
+        log_in user
+        @current_user = user
       end
-    end
-  end
-
-  # 現在ログイン中のユーザーを返す（いる場合）
-  def current_user
-    if session[:user_id]
-      @current_user ||= User.find_by(id: session[:user_id])
     end
   end
 
