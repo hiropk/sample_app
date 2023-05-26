7cdde457952cf7690337679044b04b9f2a40b14d
hiropk <kano@netlab.jp>
2023-05-26 15:33:10 +0900

    Add account activation

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,5 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
   # 本番環境では、user@realdomain.comを自分のメールアドレスに変える
-  default from: "user@realdomain.com" 
+  default from: "kano@netlab.jp" 
   layout "mailer"
 end
